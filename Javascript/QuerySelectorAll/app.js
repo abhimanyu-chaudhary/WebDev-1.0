c0b9ef6adca4ead974c4db2acee5e5930d5d8481
Abhimanyu Chaudhary <146760746+abhimanyu-chaudhary@users.noreply.github.com>
2023-12-20 16:40:18 +0530

    Popup

--- Javascript/QuerySelectorAll/app.js
+++ Javascript/QuerySelectorAll/app.js
@@ -2,16 +2,32 @@
 
 let foodItems = document.querySelectorAll('.card');
 foodItems.forEach((item)=>{
-    item.addEventListener('click', (e)=> {
-            console.log(item.dataset.food, item.dataset.price);
-            openPopup();
-            })
+    item.addEventListener('click', ()=> {
+          console.log(item.dataset.food, item.dataset.price);
+          openPopup(item.dataset.food);
+          orderValue(item.dataset.price);
+        })
 })
 
-function openPopup() {
+function openPopup(order) {
     document.getElementById('popup-display').style.display = 'flex';
+    document.getElementById('order').innerText = order + " is added in your cart";
   }
+
+  function orderValue(price){
+    if(price>100){
+    //  document.getElementById('price').innerText = "Your order is of value " + price + "Delivery Free For You";
+     console.log('FREE DELIVERY');
+    }
+    else{
+    //  document.getElementById('price').innerText = "Your order is of value " + price + "Make it of 100 for FREE DELIVERY";
+     console.log('PAID DELIVERY');
+    }
+ }
+
+
 
   function closePopup() {
     document.getElementById('popup-display').style.display = 'none';
   }
+
